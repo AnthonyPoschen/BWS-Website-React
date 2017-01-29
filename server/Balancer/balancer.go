86d5ce58dc1305fec08dbd0495017256dfa640f3
Anthony Poschen <anthony_@live.com.au>
2017-01-29 18:11:55 +1100

    added forwarding backend StatusCode's through balancer.

--- server/Balancer/balancer.go
+++ server/Balancer/balancer.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+var bLocalDev = false
+var api = "api"
+var webfront = "webfront"
+
 // Init the balancer and start the server listener.
 func main() {
 
+	flag.BoolVar(&bLocalDev, "local", false, "If Present Uses localhost instead of service names")
+	flag.Parse()
+	if bLocalDev {
+		api = "localhost"
+		webfront = "localhost"
+	}
 	http.HandleFunc("/api", apiHandler)
 	http.HandleFunc("/api/", apiHandler)
 	http.HandleFunc("/", webpageHandler)
@@ -18,32 +29,38 @@ func main() {
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
 	// forward this to the api backend
-	resp, err := http.Get("http://api:8001/api")
+
+	resp, err := http.Get("http://" + api + ":8001" + r.URL.String())
+	defer resp.Body.Close()
 	if err != nil {
 		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	defer resp.Body.Close()
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(resp.StatusCode)
 	w.Write(data)
 }
 
 func webpageHandler(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("http://webfront:8002" + r.URL.Path)
+	resp, err := http.Get("http://" + webfront + ":8002" + r.URL.Path)
+	defer resp.Body.Close()
 	if err != nil {
 		log.Println(err)
 		// return a 404 later?
 		return
 	}
-	defer resp.Body.Close()
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	w.WriteHeader(resp.StatusCode)
 	w.Write(data)
 }
